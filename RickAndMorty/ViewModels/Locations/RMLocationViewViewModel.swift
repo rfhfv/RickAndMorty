a453a06caa9728797be0cab36f853e560c4ef5c1
Anna <sozontova.an@icloud.com>
2026-06-20 01:09:57 +0700

    Create RMTableLoadingFooterView

--- RickAndMorty/ViewModels/Locations/RMLocationViewViewModel.swift
+++ RickAndMorty/ViewModels/Locations/RMLocationViewViewModel.swift
@@ -6,6 +6,14 @@ protocol RMLocationViewModelDelegate: AnyObject {
 
 final class RMLocationViewViewModel {
     
+    public var shouldShowLoadMoreIndicator: Bool {
+        return apiInfo?.next != nil
+    }
+    
+    public var isLoadingMoreLocations: Bool = false
+    
+    public private(set) var cellViewModels: [RMLocationViewTableViewCellViewModel] = []
+    
     weak var delegate: RMLocationViewModelDelegate?
     
     private var locations: [RMLocation] = [] {
@@ -20,10 +28,52 @@ final class RMLocationViewViewModel {
         }
     }
     
-    public private(set) var cellViewModels: [RMLocationViewTableViewCellViewModel] = []
     private var apiInfo: RMGetAllLocationsReponse.Info?
     
+    // MARK: - Init
+    
     init() {}
+    
+    public func fetchAdditionalLocations() {
+        guard !isLoadingMoreLocations else {
+            return
+        }
+        
+        guard let nextUrlString = apiInfo?.next,
+              let url = URL(string: nextUrlString) else {
+            return
+        }
+        
+        isLoadingMoreLocations = true
+        
+        guard let request = RMRequest(url: url) else {
+            isLoadingMoreLocations = false
+            return
+        }
+        
+        RMService.shared.execute(
+            request,
+            expecting: RMGetAllLocationsReponse.self) { [weak self] result in
+                guard let self else { return }
+                
+                switch result {
+                case .success(let responseModel):
+                    let moreResults = responseModel.results
+                    let info = responseModel.info
+                    self.apiInfo = info
+                    self.cellViewModels.append(contentsOf: moreResults.compactMap({
+                        return RMLocationViewTableViewCellViewModel(location: $0)
+                    }))
+                    
+                    DispatchQueue.main.async {
+                        self.isLoadingMoreLocations = false
+                    }
+                case .failure(let error):
+                    print(String(describing: error))
+                    self.isLoadingMoreLocations = false
+                }
+            }
+    }
     
     public func location(at index: Int) -> RMLocation? {
         guard index < locations.count, index >= 0 else {
